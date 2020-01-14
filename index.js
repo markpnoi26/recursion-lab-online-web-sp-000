1468341184fa963de533b8fd22216e0fdfd67fc4
Mark Daniel  Delgado <markpnoi26@gmail.com>
2020-01-14 03:18:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
 // Code your solution here!
 
+function printString(myString) {
+  console.log(myString[0]);
+ 
+  if (myString.length > 1) {
+    let mySubString = myString.substring(1, myString.length);
+    printString(mySubString);
+  } else {
+    return true;
+  }
+}
+
+function reverseString(string) {
+  let finalChar = string[string.length - 1]
+  
+  if (string.length > 1) {
+    let substring = string.substring(0, string.length - 1)
+    return(finalChar + reverseString(substring))
+  } else {
+    return string[0]
+  }
+}
+
+function isPalindrome(string) {
+  
+}
